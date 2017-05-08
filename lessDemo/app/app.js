6ab6e3dfa5d0b40b0451f2e559c8d1ec92b3b0bd
linqiudi@langlib.com <linqiudi@langlib.com>
2017-05-08 15:16:17 +0800

    feat(lessDemo):修改 jade 模板渲染 html 报错问题

--- lessDemo/app/app.js
+++ lessDemo/app/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var swig = require('swig');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +14,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
+// 将 ejs 转化为 html（routes 需要加后缀名）
+app.engine('html', require('ejs').renderFile);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,6 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 关联 routes 层相关的访问
 app.use('/', index);
 app.use('/users', users);
 
