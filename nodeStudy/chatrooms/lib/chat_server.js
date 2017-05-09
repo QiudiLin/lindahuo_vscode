570fb2b4ed49f468deb71b0ff5fcee7ce62d54df
qtel <413144596@qq.com>
2017-05-09 08:27:43 +0800

    feat(chatroom):重新梳理代码

--- nodeStudy/chatrooms/lib/chat_server.js
+++ nodeStudy/chatrooms/lib/chat_server.js
@@ -1,127 +1,112 @@
-const socketio = require('socket.io');
+var socketio = require('socket.io');
+var io;
+var guestNumber = 1;
+var nickNames = {};
+var namesUsed = [];
+var currentRoom = {};
 
-let io;
-let guestNumber = 1;
-let nickNames = {};
-let namesUsed = [];
-let currentRoom = {};
-
-exports.listen = function (server) {
-    // 启动 socket io 服务器，允许它搭载在已有的 HTTP 服务器上
-    io = socketio.listen(server);
-    io.set('log leve', 1);
-
-    io.sockets.on('connection', (socket) => {
-        guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
-        // 在用户连接上来时把他放入聊天室 Lobby 里
-        joinRoom(socket, 'Lobby');
-        // 处理用户的消息，更名，以及聊天室的创建和变更
-        handleMessageBroadcasting(socket, nickNames);
-        handleNameChangeAttempts(socket, nickNames, namesUsed);
-        handleRoomJoining(socket);
-        // 用户发出请求时，向其提供已经被占用的聊天室的列表
-        socket.on('rooms', function () {
-            socket.emit('rooms', io.sockets.manager.rooms);
-        });
-        // 定义用户断开连接后的清除逻辑
-        handleClientDisconnection(socket, nickNames, namesUsed);
+exports.listen = function(server) {
+  io = socketio.listen(server);
+  io.set('log level', 1);
+  io.sockets.on('connection', function (socket) {
+    guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
+    joinRoom(socket, 'Lobby');
+    handleMessageBroadcasting(socket, nickNames);
+    handleNameChangeAttempts(socket, nickNames, namesUsed);
+    handleRoomJoining(socket);
+    socket.on('rooms', function() {
+      socket.emit('rooms', io.sockets.manager.rooms);
     });
+    handleClientDisconnection(socket, nickNames, namesUsed);
+  });
 };
 
-// 分配用户昵称
 function assignGuestName(socket, guestNumber, nickNames, namesUsed) {
-    const name = 'Guest' + guestNumber;
-    nickNames[socket.id] = name;
-    socket.emit('nameResult', {
-        success: true,
-        name: name
-    });
-    namesUsed.push(name);
+  var name = 'Guest' + guestNumber;
+  nickNames[socket.id] = name;
+  socket.emit('nameResult', {
+    success: true,
+    name: name
+  });
+  namesUsed.push(name);
+  return guestNumber + 1;
+}
 
-    return guestNumber + 1;
-};
-
-// 与进入聊天室相关的逻辑
 function joinRoom(socket, room) {
-    socket.join(room);
-    currentRoom[socket.io] = room;
-    socket.emit('joinResult', { room: room });
-    socket.broadcast.to(room).emit('message', {
-        text: nickNames[socket.id] + ' has joined ' + room + '.'
-    });
+  socket.join(room);
+  currentRoom[socket.id] = room;
+  socket.emit('joinResult', {room: room});
+  socket.broadcast.to(room).emit('message', {
+    text: nickNames[socket.id] + ' has joined ' + room + '.'
+  });
 
-    const usersInRoom = io.sockets.clients(room);
-    // 如果房间用户人数大于 1，汇总人数
-    if (usersInRoom.length > 1) {
-        const usersInRoomSummary = 'Users currently in ' + room + ': ';
-        for (let index in usersInRoom) {
-            const userSocketId = usersInRoom[index].id;
-            if (userSocketId != socket.id) {
-                if (index > 0) {
-                    usersInRoomSummary += ', ';
-                }
-                usersInRoomSummary += nickNames[userSocketId];
-            }
-            usersInRoomSummary += '.';
-            socket.emit('message', { text: usersInRoomSummary });
+  var usersInRoom = io.sockets.clients(room);
+  if (usersInRoom.length > 1) {
+    var usersInRoomSummary = 'Users currently in ' + room + ': ';
+    for (var index in usersInRoom) {
+      var userSocketId = usersInRoom[index].id;
+      if (userSocketId != socket.id) {
+        if (index > 0) {
+          usersInRoomSummary += ', ';
         }
+        usersInRoomSummary += nickNames[userSocketId];
+      }
     }
-};
+    usersInRoomSummary += '.';
+    socket.emit('message', {text: usersInRoomSummary});
+  }
+}
 
-// 更改请求的处理逻辑
 function handleNameChangeAttempts(socket, nickNames, namesUsed) {
-    socket.on('nameAttempt', (name) => {
-        if (name.indexOf('Guest') == 0) {
-            socket.emit('nameResult', {
-                success: false,
-                message: 'Names cannot begin with "Guest"'
-            });
-        } else {
-            if (namesUsed.indexOf(name) == -1) {
-                const previousName = nickNames[socket.id];
-                const previousNameIndex = namesUsed.indexOf(previousName);
-                namesUsed.push(name);
-                nickNames[socket.id] = name;
-                delete namesUsed[previousNameIndex];
-                socket.emit('nameResult', {
-                    success: true,
-                    name: name
-                });
-                socket.broadcast.to(currentRoom[socket.id]).emit('message', {
-                    text: previousName + ' is now know as ' + name + '.'
-                });
-            } else {
-                socket.emi('nameResult', {
-                    success: false,
-                    message: 'That name is already in use.'
-                });
-            }
-        }
-    });
-};
-
-// 发起聊天信息
-function handleMessageBroadcasting(socket) {
-    socket.on('message', (message) => {
-        socket.broadcast.to(messsage.room).emit('message', {
-            text: nickNames[socket.id] + ': ' + message.text
+  socket.on('nameAttempt', function(name) {
+    if (name.indexOf('Guest') == 0) {
+      socket.emit('nameResult', {
+        success: false,
+        message: 'Names cannot begin with "Guest".'
+      });
+    } else {
+      if (namesUsed.indexOf(name) == -1) {
+        var previousName = nickNames[socket.id];
+        var previousNameIndex = namesUsed.indexOf(previousName);
+        namesUsed.push(name);
+        nickNames[socket.id] = name;
+        delete namesUsed[previousNameIndex];
+        socket.emit('nameResult', {
+          success: true,
+          name: name
         });
-    });
-};
+        socket.broadcast.to(currentRoom[socket.id]).emit('message', {
+          text: previousName + ' is now known as ' + name + '.'
+        });
+      } else {
+        socket.emit('nameResult', {
+          success: false,
+          message: 'That name is already in use.'
+        });
+      }
+    }
+  });
+}
 
-// 创建房间
+function handleMessageBroadcasting(socket) {
+  socket.on('message', function (message) {
+    socket.broadcast.to(message.room).emit('message', {
+      text: nickNames[socket.id] + ': ' + message.text
+    });
+  });
+}
+
 function handleRoomJoining(socket) {
-    socket.on('join', (room) => {
-        socket.leave(currentRoom[socket.id]);
-        joinRoom(socket, room.newRoom);
-    });
-};
+  socket.on('join', function(room) {
+    socket.leave(currentRoom[socket.id]);
+    joinRoom(socket, room.newRoom);
+  });
+}
 
-// 用户断开连接
 function handleClientDisconnection(socket) {
-    socket.on('disconnect', () => {
-        const nameIndex = namesUsed.indexOf(nickNames[socket.id]);
-        delete namesUsed[nameIndex];
-        delete nickNames[socket.id];
-    });
-};
+  socket.on('disconnect', function() {
+    var nameIndex = namesUsed.indexOf(nickNames[socket.id]);
+    delete namesUsed[nameIndex];
+    delete nickNames[socket.id];
+  });
+}
--- nodeStudy/chatrooms/public/javascripts/chat_ui.js
+++ nodeStudy/chatrooms/public/javascripts/chat_ui.js
@@ -1,83 +1,79 @@
-// 显示系统创建的内容信息
 function divEscapedContentElement(message) {
-    return $('<div></div>').text(message);
-};
-function divSystemContentElement(message) {
-    return $('<div></div>').html('<i>' + message + '</i>')
-};
+  return $('<div></div>').text(message);
+}
 
-//处理原始的用户输入
+function divSystemContentElement(message) {
+  return $('<div></div>').html('<i>' + message + '</i>');
+}
+
 function processUserInput(chatApp, socket) {
-    const message = $('#send-message').val();
-    let systemMessage;
-    // 如果用户输入的内容以斜杠开头，将其作为聊天命令
-    if (message.charAt(0) == '/') {
-        systemMessage = chatApp.processCommand(message);
-        if (systemMessage) {
-            $('#messages').append(divSystemContentElement(systemMessage));
-        }
+  var message = $('#send-message').val();
+  var systemMessage;
+
+  if (message.charAt(0) == '/') {
+    systemMessage = chatApp.processCommand(message);
+    if (systemMessage) {
+      $('#messages').append(divSystemContentElement(systemMessage));
+    }
+  } else {
+    chatApp.sendMessage($('#room').text(), message);
+    $('#messages').append(divEscapedContentElement(message));
+    $('#messages').scrollTop($('#messages').prop('scrollHeight'));
+  }
+
+  $('#send-message').val('');
+}
+
+var socket = io.connect();
+
+$(document).ready(function() {
+  var chatApp = new Chat(socket);
+
+  socket.on('nameResult', function(result) {
+    var message;
+
+    if (result.success) {
+      message = 'You are now known as ' + result.name + '.';
     } else {
-        // 将非命令输入广播给其他用户
-        chatApp.sendMessage($('#room').text(), message);
-        $('#messages').append(divEsapedContentElement(message));
-        $('#messages').scrollTop($('#messages').prop('scrollHeight'));
+      message = result.message;
+    }
+    $('#messages').append(divSystemContentElement(message));
+  });
+
+  socket.on('joinResult', function(result) {
+    $('#room').text(result.room);
+    $('#messages').append(divSystemContentElement('Room changed.'));
+  });
+
+  socket.on('message', function (message) {
+    var newElement = $('<div></div>').text(message.text);
+    $('#messages').append(newElement);
+  });
+
+  socket.on('rooms', function(rooms) {
+    $('#room-list').empty();
+
+    for(var room in rooms) {
+      room = room.substring(1, room.length);
+      if (room != '') {
+        $('#room-list').append(divEscapedContentElement(room));
+      }
     }
 
-    $('#send-message').val('');
-};
-
-// 客户单程序初始化逻辑
-
-const socket = io.connect();
-
-$(document).ready(() => {
-    let chatApp = new Chat(socket);
-
-    socket.on('nameResult', (result) => {
-        let message;
-
-        if (result.success) {
-            message = 'You are now known as ' + result.name + '.';
-        } else {
-            message = result.message;
-        }
-        $('#messages').append(divSystemContentElement(message));
+    $('#room-list div').click(function() {
+      chatApp.processCommand('/join ' + $(this).text());
+      $('#send-message').focus();
     });
+  });
 
-    socket.on('joinResult', (reuslt) => {
-        $('#room').text(result.room);
-        $('#messages').append(divSystemContentElement('Room changed.'));
-    });
+  setInterval(function() {
+    socket.emit('rooms');
+  }, 1000);
 
-    socket.on('message', (message) => {
-        const newElement = $('<div></div>').text(message.text);
-        $('#messages').append(newElement);
-    });
+  $('#send-message').focus();
 
-    socket.on('rooms', (rooms) => {
-        $('#room-list').empty();
-
-        for (let room in rooms) {
-            room = room.substring(1, room.length);
-            if (room != '') {
-                $('#room-list').append(divEscapedContentElement(room));
-            }
-         }
-         $('#room-list div').click(() => {
-            chatApp.processCommand('/join ' + $(this).text());
-            $('#send-message').focus();
-         });
-    });
-
-    setInterval(() => {
-        socket.emit('room');
-    }, 1000);
-
-    $('#send-message').focus();
-
-    $('#send-message').submit(() => {
-        processUserInput(chatApp, socket);
-
-        return false;
-    });
+  $('#send-form').submit(function() {
+    processUserInput(chatApp, socket);
+    return false;
+  });
 });
